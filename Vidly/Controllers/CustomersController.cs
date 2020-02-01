5a32362b7299fb2f2a6d2df82ad8d45ed06cbb33
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2020-02-01 15:25:58 +0200

    Add project files.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+
+        List<Customer> customers = new List<Customer>
+            {
+                new Customer{ID = 1,Name = "Mohamed"},
+                new Customer{ID = 2,Name="Ahmed"}
+        };
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+
+
+            return View(customers.ToList());
+        }
+
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                var selectedcustomer = customers.Where(c => c.ID == id).Single();
+                return View(selectedcustomer);
+
+            }
+            catch (ArgumentNullException e)
+            {
+
+                return HttpNotFound();
+            }
+            catch (InvalidOperationException e)
+            {
+                return HttpNotFound();
+            }
+
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        List<Movie> movies = new List<Movie>
+            {
+                 new Movie {ID = 1, Name = "Shrek" },
+                 new Movie {ID = 2,Name="john Wick"}
+            };
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+
+            return View(movies.ToList());
+        }
+
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                var selectedMovie = movies.Where(m => m.ID == id).SingleOrDefault();
+                return View(selectedMovie);
+            }
+            catch (InvalidOperationException e)
+            {
+
+                return HttpNotFound();
+            }
+            catch (ArgumentNullException e)
+            {
+                return HttpNotFound();
+            }
+        }
+    }
+}
