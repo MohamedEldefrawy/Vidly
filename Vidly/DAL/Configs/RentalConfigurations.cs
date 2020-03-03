100cacef0a422efcf7b8bfcff5a75f98d337f199
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2020-03-03 18:42:04 +0200

    Create Configurations To Employee Rental And REntal Details Tabels

---
+++ Vidly/DAL/Configs/RentalConfigurations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using Vidly.BL.Domain;
+
+namespace Vidly.DAL.Configs
+{
+    public class RentalConfigurations : EntityTypeConfiguration<Rental>
+    {
+        public RentalConfigurations()
+        {
+            Property(r => r.RentDate)
+                .IsRequired()
+                .HasColumnType("Date");
+        }
+    }
+}
---
+++ Vidly/DAL/Configs/RentalDetailsConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using Vidly.BL.Domain;
+
+namespace Vidly.DAL.Configs
+{
+    public class RentalDetailsConfigurations : EntityTypeConfiguration<RentalDetails>
+    {
+        public RentalDetailsConfigurations()
+        {
+            Property(r => r.quantity)
+                .IsRequired();
+            Property(r => r.unitPrice)
+                .IsRequired()
+                .HasColumnType("Decimal(9,2)");
+        }
+    }
+}
